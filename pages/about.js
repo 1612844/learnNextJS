2e271b3833b18dd11375a2039dfba3a62af6e5c3
Huynh Ngoc Dinh <huynhngocdinh98@gmail.com>
2020-02-24 07:27:11 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import Header from '../components/Header';
+import Layout from '../components/MyLayout';
+
+export default () => {
+    return (
+        <Layout>
+            <h1>About pages</h1>
+        </Layout>
+    );
+}
